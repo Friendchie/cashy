a0854c1d27282abc4500eb488b833c0c6b1d49c3
Ethan Martinez <ethanmartinez2503@gmail.com>
2026-09-09 20:41:21 -0500

    feat: add start.bat and test.bat for 1-click Windows execution

---
+++ test.bat
@@ -0,0 +1,6 @@
+@echo off
+set "PATH=%PATH%;C:\Program Files\nodejs"
+echo ======================================================
+echo 🧪 Ejecutando Suite de Auditoria y Pruebas
+echo ======================================================
+npm.cmd test
